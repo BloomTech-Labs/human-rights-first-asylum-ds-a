9d14a04fa8b01404fa15d253186cb6c9f5fb49ff
StevenBryceLee <stevenl451@gmail.com>
2020-12-18 16:01:32 -0800

    aws deploy

--- app/test.py
+++ app/test.py
@@ -7,19 +7,8 @@ import pytesseract
 import sqlalchemy
 from sqlalchemy import create_engine
 
-
-
-
-
-
-
-router = APIRouter()
-
-
 load_dotenv()
-
-
 database_url = os.getenv('DATABASE_URL')
 
-
 print('db url name', database_url)
+
