9c07f90b1ac8f0fae6647ef83f888a6936ff8d88
StevenBryceLee <stevenl451@gmail.com>
2020-12-22 14:40:52 -0800

    deploy

--- app/test.py
+++ app/test.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from fastapi import APIRouter, File
 from pdf2image import convert_from_bytes
 from PIL import Image
-import pytesseract
+# import pytesseract
 import sqlalchemy
 from sqlalchemy import create_engine
 
@@ -13,4 +13,8 @@ database_url = os.getenv('DATABASE_URL')
 print('db url name', database_url)
 engine = create_engine(database_url)
 selQuery = """SELECT COUNT(*) from pdfs;"""
-print(engine.execute(selQuery).fetchAll())
+truncateQuery = """TRUNCATE TABLE pdfs"""
+# print(engine.execute(selQuery))
+print('initial values prior to wipe', engine.execute(selQuery).fetchall())
+# engine.execute(truncateQuery)
+print(engine.execute(selQuery).fetchall())
