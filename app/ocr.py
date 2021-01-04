30ce8e35aba7a0b688fb2a871cdba762f022526e
StevenBryceLee <stevenl451@gmail.com>
2021-01-04 10:28:36 -0800

    adding file name print

--- app/ocr.py
+++ app/ocr.py
@@ -63,6 +63,7 @@ def ocr_func(pdfBytes: bytes = File(...), txt_folder: str = './temp/'):
  
     for i in range(num_pages):
         filename = 'page_' + str(i) + '.jpg'
+        print('fileName')
         img = Image.open(filename).resize((10,10), Image.ANTIALIAS)
         result = (pytesseract.image_to_string(img))
         text = str(result)
