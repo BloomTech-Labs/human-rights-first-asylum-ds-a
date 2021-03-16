122fcf78ef73e3eec2eb473ab63f1674e257b0d7
boscolio <nickrules@gmail.com>
2021-03-16 15:18:15 -0500

    Updated current json dict names in ocr.py to match web side. Reimplemented case_filed_within_one_year in BIA_Scraper.py

--- app/BIA_Scraper.py
+++ app/BIA_Scraper.py
@@ -664,70 +664,70 @@ class BIACase:
 
     def get_applicant_determined_credibility(self) -> str:
         return "test"
-#         '''
-#         • Returns the judge's decision on whether the applicant is a credible witness.
-#         Curently, the field's output is dependent on occurance of specific tokens 
-#         in the document; this method needs to be fine-tuned and validated.
-#         '''
+        # '''
+        # • Returns the judge's decision on whether the applicant is a credible witness.
+        # Curently, the field's output is dependent on occurance of specific tokens 
+        # in the document; this method needs to be fine-tuned and validated.
+        # '''
 
-#         credibility = [
-#             'credible'
-#         ]
+        # credibility = [
+        #     'credible'
+        # ]
 
-#         similar_cred: Callable[[str, float], Union[str, None]]
-#         similar_cred = similar_in_list(credibility)
+        # similar_cred: Callable[[str, float], Union[str, None]]
+        # similar_cred = similar_in_list(credibility)
 
-#          for token in self.doc:
+        #  for token in self.doc:
 
-#             sent: str
-#             sent = token.sent.text.lower()
+        #     sent: str
+        #     sent = token.sent.text.lower()
 
-#             s: Union[str, None]
-#             s = similar_cred(token.text.lower(), 0.9)
+        #     s: Union[str, None]
+        #     s = similar_cred(token.text.lower(), 0.9)
 
-#             if s == 'credible':
-#                 prev_word = self.doc[token.i-1].text.lower()
-#                 next_word = self.doc[token.i+1].text.lower()
-#                 if not similar(prev_word, 'not', 0.95) \
-#                     and not similar(next_word, 'witness', 0.95): 
-#                     return 'Yes'
-#                 else:
-#                     return 'No'
-#                 if s not in self.doc:
-#                     return 'N/A to case'
+        #     if s == 'credible':
+        #         prev_word = self.doc[token.i-1].text.lower()
+        #         next_word = self.doc[token.i+1].text.lower()
+        #         if not similar(prev_word, 'not', 0.95) \
+        #             and not similar(next_word, 'witness', 0.95): 
+        #             return 'Yes'
+        #         else:
+        #             return 'No'
+        #         if s not in self.doc:
+        #             return 'N/A to case'
     
     def check_for_one_year(self) -> bool:
-        return True
-    #     """
-    #     Checks whether or not the asylum-seeker argued to be exempt from the
-    #     one-year guideline.  Specifically, it checks to see if the document
-    #     contains either "changed circumstance" or "extraordinary circumstance". 
-    #     If it does, and one of the five terms ("year", "delay", "time",
-    #     "period", "deadline") is within 10 lemmas, then the function
-    #     returns True.  Otherwise, it returns False.
-    #     """
-    #     # If one of the four context words are w/in 100 characters of the
-    #     # phrase, we conclude that it is related to the one-year rule
-    #     lemma_list = [token.lemma_.lower() for token in self.doc]
-    #     for idx in range(0,len(lemma_list)):
-    #         if (lemma_list[idx] == 'change') \
-    #             & (lemma_list[idx + 1] == 'circumstance'):
-    #             idx_start = lemma_list.index('change')
-    #             idx_end  = idx_start + 1
-    #             search_list = [lemma for lemma in \
-    #                 lemma_list[idx_start - 10: idx_end + 10]]
-    #             if any(term in search_list \
-    #                 for term in ('year','delay','time','period','deadline')):
-    #                 return True
-    #     # Do the same for "extraordinary circumstance"
-    #     for idx in range(0,len(lemma_list)):
-    #         if (lemma_list[idx] == 'extraordinary') \
-    #             & (lemma_list[idx + 1] == 'circumstance'):
-    #             idx_start = lemma_list.index('change')
-    #             idx_end  = idx_start + 1
-    #             search_list = [lemma for lemma in \
-    #                 lemma_list[idx_start - 10: idx_end + 10]]
-    #             if any(term in search_list for term in \
-    #                 ('year','delay','time','period','deadline')):
-    #                 return True
-    #     return False
+        # return True
+        """
+        Checks whether or not the asylum-seeker argued to be exempt from the
+        one-year guideline.  Specifically, it checks to see if the document
+        contains either "changed circumstance" or "extraordinary circumstance". 
+        If it does, and one of the five terms ("year", "delay", "time",
+        "period", "deadline") is within 10 lemmas, then the function
+        returns True.  Otherwise, it returns False.
+        """
+        # If one of the four context words are w/in 100 characters of the
+        # phrase, we conclude that it is related to the one-year rule
+        lemma_list = [token.lemma_.lower() for token in self.doc]
+        for idx in range(0,len(lemma_list)):
+            if (lemma_list[idx] == 'change') \
+                & (lemma_list[idx + 1] == 'circumstance'):
+                idx_start = lemma_list.index('change')
+                idx_end  = idx_start + 1
+                search_list = [lemma for lemma in \
+                    lemma_list[idx_start - 10: idx_end + 10]]
+                if any(term in search_list \
+                    for term in ('year','delay','time','period','deadline')):
+                    return True
+        # Do the same for "extraordinary circumstance"
+        for idx in range(0,len(lemma_list)):
+            if (lemma_list[idx] == 'extraordinary') \
+                & (lemma_list[idx + 1] == 'circumstance'):
+                idx_start = lemma_list.index('change')
+                idx_end  = idx_start + 1
+                search_list = [lemma for lemma in \
+                    lemma_list[idx_start - 10: idx_end + 10]]
+                if any(term in search_list for term in \
+                    ('year','delay','time','period','deadline')):
+                    return True
+        return False
