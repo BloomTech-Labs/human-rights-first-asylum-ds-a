083a439ab05f567b86c2283aaf08eaecf72ac18f
Jace Hambrick <63068541+Jace-Hambrick@users.noreply.github.com>
2021-03-18 13:51:18 -0700

    Add files via upload

---
+++ visualizations/visuals.py
@@ -0,0 +1,69 @@
+# DATA ETHICS -------------------------------------------------------------------------------------------------
+# For the most part court records are public information.
+# Try to keep more private information off the records.
+# Possibly keep first names off, if people are that interested let them look online.
+#
+# Things to know
+#   a) obtain informed consent from potential research participants
+#              In most legal cases, court records are public. But be safe and double check.
+#
+#   b) minimise the risk of harm to participants
+#              The Human Rights First Asylum is not trying to put people at risk or bring them harm.
+#
+#   c) protect their anonymity and confidentiality
+#              Again. HRFA isn't there to put people at risk or bring them harm. However, it is possible
+#              that by putting their information on site it may cause them complications. Don't use pictures,
+#              FIRST and LAST names in conjunction, DOB information, as this may lead to legal complications for HRFA.
+#
+#   d) avoid using deceptive practices
+#
+#   e) give participants the right to withdraw from your research
+#              Note: bring this up to Shareholders. It's possible to gain animosity by not allowing someone to withdraw
+#              the information you are using about them. 
+#
+#
+#       These 'ethics' vary from source to source. But stay relatively the same. 
+#       - Site used (https://dissertation.laerd.com/principles-of-research-ethics.php)
+#
+
+#
+# VISUALIZATION IDEAS -----------------------------------------------------------------------------------------
+#   SOFTWARES--------------
+#
+# > Use Matplotlib extension Basemap (https://matplotlib.org/basemap/)
+#   Using Basemap, we can plot coastal lines and countries directly.
+#
+#      Found example/walkthrough 
+#      (https://medium.com/analytics-vidhya/how-to-plot-data-on-a-world-map-in-python-25cf9733c3dd)
+# 
+# > Using Folium (https://python-visualization.github.io/folium/)
+#   Which creates an interactive map which you can add markers too
+#
+#       Found example/walkthrough
+#       (https://www.earthdatascience.org/courses/scientists-guide-to-plotting-data-in-python/plot-spatial-data/customize-raster-plots/interactive-maps/)
+#
+# > Using Plotly open source graphing (https://plotly.com/)
+#   Can be installed via AWS(?)
+#
+#       Found example/walkthrough(https://plotly.com/python/maps/)
+#
+#
+#
+#
+#
+#    GRAPH IDEAS-----------------------------------------------------------------------------------------------
+# 
+# World map with Green (won cases), Grey (awaiting verdict/ not yet to trial), and Red (lost cases) dots representing cases across the nation
+# 
+# - information to show on a basic level
+#       COUNTRY OF ORGIN
+#       DATE
+#       OUTCOME
+#
+# 
+# Bar Chart showing number of male/female cases seeking asylums
+#
+# Heatmap showing influx of Asylum Seekers from regions of the world
+# 
+# Demographic Graph showing Ages/Races/Gender that have been granted/denied asylum.
+#
