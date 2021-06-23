5c607d9d1c7297d05b23b994c583299b6106c828
BrokenShell <webmaster@sharpdesigndigital.com>
2021-06-23 12:17:20 -0700

    fixes single quote SQL bug

--- scratch.py
+++ scratch.py
@@ -2,14 +2,12 @@ import requests
 
 
 arr = [
-    # "004dcaad-8c41-403f-96cc-ff4db68c45d7",
-    # "00c46a50-84ee-4717-97bf-929d3a767501",
-    # "00c76f3f-c4e2-49b8-9adf-c1044302627d",
-    # "00ef179e-ad1d-474f-bd36-97b18aac452e",
-    # "01108e14-b94a-4f59-9a76-9f8cb2f002c2",
-    # "012604c9-fdf5-4452-8cb2-de7a594354fa",
-    # "013a6a6a-40d1-4c29-97c4-5aa865ca5a42",
-    # "01881766-7409-43a9-a6bd-fa392c87cbe4",
+    "00c76f3f-c4e2-49b8-9adf-c1044302627d",
+    "00ef179e-ad1d-474f-bd36-97b18aac452e",
+    "03d8f05d-f60a-4f31-b2cc-5a3da5c3c59b",
+    "0333dc84-ea98-4be8-85dc-5c8f0410af20",
+    "0563a753-aefd-4026-8d22-a24169132933",
+    "01881766-7409-43a9-a6bd-fa392c87cbe4",
 ]
 
 for uuid in arr:
