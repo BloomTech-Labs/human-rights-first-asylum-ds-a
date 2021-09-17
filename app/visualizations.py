c47df1dfb952447d1d8d819017680add1eb94d6f
Lucas Chatham <chathamworks@gmail.com>
2021-09-17 13:35:48 -0700

    v.1 of the new data viz
    This is v.1 of the new data viz for the judge's page. This first PR needs to be tested by the web team to ensure the DS and FE/BE is correctly working together.

--- app/visualizations.py
+++ app/visualizations.py
@@ -1,24 +1,43 @@
+
+from plotly.graph_objects import Figure
 import plotly.graph_objects as go
 import plotly.express as px
-from app.db_ops import get_judge_df
+from app.db_ops import get_judge_df, get_table, get_df
+import pandas as pd
 
+# def get_judge_side_bar(judge_name: str) -> go.Figure:
+#         """
+#         Function grabs data from database by judge and creates a
+#         side-by-side bar chart for visualization.
+#         Input: judge_name (name of judge to filter by)
+#         Output: fig (object with visualization data)
+#         """
+#         df = get_judge_df(judge_name)
+#         df["count"] = 1
+#         grouped_df = df.groupby(by=["protected_grounds",
+#                                     "outcome"]).agg({
+#                                                     "count": ["sum"]
+#                                                     }).reset_index()
+#         fig = px.bar(grouped_df, x="protected_grounds", y="count",
+#                      color="outcome",
+#                      color_discrete_map={"granted": '#00D100',
+#                                          "denied": "#D10000"},
+#                      barmode="group", title="Side-by-Side Bar Chart")
+#         return fig
 
-def get_judge_side_bar(judge_name: str) -> go.Figure:
-        """
-        Function grabs data from database by judge and creates a
-        side-by-side bar chart for visualization.
-        Input: judge_name (name of judge to filter by)
-        Output: fig (object with visualization data)
-        """
-        df = get_judge_df(judge_name)
-        df["count"] = 1
-        grouped_df = df.groupby(by=["protected_grounds",
-                                    "outcome"]).agg({
-                                                    "count": ["sum"]
-                                                    }).reset_index()
-        fig = px.bar(grouped_df, x="protected_grounds", y="count",
-                     color="outcome",
-                     color_discrete_map={"granted": '#00D100',
-                                         "denied": "#D10000"},
-                     barmode="group", title="Side-by-Side Bar Chart")
-        return fig
+def get_judge_vis(judge_id: int) -> Figure:
+    df = get_df()
+    judge_df = df[df["judge_id"] == judge_id]
+    cross_tab = pd.crosstab(
+        judge_df["protected_grounds"],
+        judge_df["outcome"],
+    )
+    data = [
+        go.Bar(name=col, x=cross_tab.index, y=cross_tab[col])
+        for col in cross_tab.columns
+    ]
+    layout = go.Layout(
+        title="Outcome by Protected Grounds",
+        barmode="group",
+    )
+    return go.Figure(data, layout)
